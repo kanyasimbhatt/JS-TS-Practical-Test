3a0f8a9fb282e285c111e81822929e88aa9966d6
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-04-23 15:52:22 +0530

    improve: improve code for question1

--- Typescript/question1.ts
+++ Typescript/question1.ts
@@ -1,15 +1,15 @@
 type Animal = { name: string; fly?: boolean; swim?: boolean };
 
 function canFly(animal: Animal): boolean {
-  if(animal.fly === true){
+  if ("fly" in animal && animal.fly === true) {
     return true;
   }
 
   return false;
 }
 
-const bird: Animal = { name: 'Sparrow', fly: true };
-const fish: Animal = { name: 'Salmon', swim: true };
+const bird: Animal = { name: "Sparrow", fly: true };
+const fish: Animal = { name: "Salmon", swim: true };
 
-console.log(canFly(bird));  // Output: true
-console.log(canFly(fish));  // Output: false
+console.log(canFly(bird)); // Output: true
+console.log(canFly(fish)); // Output: false
